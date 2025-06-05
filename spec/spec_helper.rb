ad69534ac1856badb99202b1d4482e71ccc3ab34
maglabaka707 <maglabaka707@gmail.com>
2025-06-05 21:42:47 +0800

    Delete spec directory

--- spec/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'spec'
-$:.unshift "#{File.dirname(__FILE__)}/../lib"
-
-require 'satisfaction'
-
-Spec::Runner.configure do |config|
-  config.prepend_before(:each){ @satisfaction = Satisfaction.new(:root => 'http://api.getsatisfaction.com') }
-end
